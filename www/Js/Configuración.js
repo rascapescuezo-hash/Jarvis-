d4c26d6bf6a2072ec3a31736dee56b96d9b62bf7
rascapescuezo-hash <rascapescuezo@gmail.com>
2026-08-06 22:14:01 -0500

    Create Configuración.js

---
+++ www/Js/Configuración.js
@@ -0,0 +1,44 @@
+// js/config.js
+export const CONFIG = {
+    palabraActivacion: 'jarvis',
+    longitudMinimaOrden: 3,
+    motorBusquedaGeneral: 'https://www.google.com/search?q=',
+    idioma: 'es-ES',
+    reinicioNormalMs: 300,
+    hablarRespuestas: true,
+    esperaSilencioOrdenMs: 1400,
+    esperaMaximaOrdenMs: 12000,
+    segundosSaltoPorDefecto: 10,
+    toleranciaActivacion: 2,
+    ventanaAntiEcoMs: 1600,
+    autoApagadoMs: 60000,
+    tamanoBoton: 46,
+    vozPitch: 1.0,
+    vozRate: 1.05
+};
+
+export const webRapidas = {
+    youtube: 'https://www.youtube.com',
+    google: 'https://www.google.com',
+    github: 'https://github.com',
+    pleisplus: 'https://www.pleisplus.com',
+    peliplus: 'https://www.pleisplus.com',
+    wikipedia: 'https://es.wikipedia.org',
+    gmail: 'https://mail.google.com',
+    whatsapp: 'https://web.whatsapp.com',
+    facebook: 'https://www.facebook.com',
+    instagram: 'https://www.instagram.com',
+    twitter: 'https://x.com',
+    x: 'https://x.com',
+    netflix: 'https://www.netflix.com',
+    spotify: 'https://open.spotify.com',
+    amazon: 'https://www.amazon.com',
+    twitch: 'https://www.twitch.tv',
+    drive: 'https://drive.google.com',
+    calendar: 'https://calendar.google.com',
+    maps: 'https://maps.google.com',
+    meet: 'https://meet.google.com',
+    classroom: 'https://classroom.google.com'
+};
+
+export const VARIANTES_ACTIVACION = ['jarvis','yarvis','harvis','jarves','jarvi','jharvis','charves','arves','arbes','chavis','jamis','garves'];
