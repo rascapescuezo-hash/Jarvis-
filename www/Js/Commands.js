41d199920ffb4d161e7c9cc8d55432bf38cd3163
rascapescuezo-hash <rascapescuezo@gmail.com>
2026-08-06 22:17:50 -0500

    Create Commands.js

---
+++ www/Js/Commands.js
@@ -0,0 +1,8 @@
+// js/commands.js
+import { CONFIG, webRapidas } from './config.js';
+import { hablar, activarJarvis, desactivarJarvis } from './speech.js';
+import { actualizarIndicador } from './ui.js';
+// ... todas las funciones auxiliares (navegar, video, clic, temporizador, etc.)
+
+// Aquí van TODAS las reglas de la versión 8 (más las nuevas)
+// Te las doy completas en el archivo adjunto.
